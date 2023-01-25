fd9581a4dcd9ef021afa2acbca951b20a2507f78
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2023-01-25 09:41:04 -0300

    se separa router

--- src/index.js
+++ src/index.js
@@ -1,75 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('./router');
 
 const app = express();
 
-const logger = require('./logger');
-const checkToken = require('./token');
-const oai = require('./openai');
-
-var router = express.Router();
-
-router.use(function (req, res, next) {
-  console.log(`${req.method}\t ${req.url}`, req.body);
-  if (!checkToken(req)) {
-    const response = { error: 'Invalid token.' };
-    logger.log(req, response);
-    return res.json(response);
-  }
-
-  if (req.url !== '/log') logger.log(req);
-
-  next();
-});
-
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(router);
-
-app.post('/ask', async (req, res, next) => {
-  const options = oai.parseOptions(req.body);
-
-  if (!options.prompt) throw new Error('Prompt is required.');
-  if (options.prompt.length < 10)
-    throw new Error('Prompt must be at least 10 characters long.');
-
-  try {
-    const answer = await oai.getAnswer(options);
-
-    console.log({ options, answer });
-
-    if (!answer || !answer[0] || !answer[0].text)
-      throw new Error('No answer found.');
-
-    res.json({ answer });
-  } catch (e) {
-    console.error(e);
-    next(e);
-  }
-});
-
-app.get('/cfg', (req, res) => {
-  try {
-    const { PORT, APP_NAME, AUTH_TOKEN } = process.env;
-    res.json({
-      PORT,
-      APP_NAME,
-      AUTH_TOKEN,
-    });
-  } catch (e) {
-    console.log(e);
-    res.json({ error: e });
-  }
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!' + process.env.APP_NAME);
-});
-
-app.get('/log', (req, res) => {
-  const log = logger.getLog();
-  res.json(log);
-});
+app.use('/api/', router);
 
 app.listen(3000, () => {
   console.log('El servidor está inicializado en el puerto 3000');
---
+++ src/router.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+
+const logger = require('./logger');
+const checkToken = require('./token');
+const oai = require('./openai');
+
+// middleware that is specific to this router
+router.use((req, res, next) => {
+  console.log(`${req.method}\t ${req.url}`, req.body);
+  if (!checkToken(req)) {
+    const response = { error: 'Invalid token.' };
+    logger.log(req, response);
+    return res.json(response);
+  }
+
+  if (req.url !== '/log') logger.log(req);
+
+  next();
+});
+
+router.post('/ask', async (req, res, next) => {
+  const options = oai.parseOptions(req.body);
+
+  if (!options.prompt) res.json({ error: 'No prompt provided.' });
+  if (options.prompt.length < 10)
+    res.json({ error: 'Prompt must be at least 10 characters long.' });
+
+  try {
+    const answer = await oai.getAnswer(options);
+
+    console.log({ options, answer });
+
+    if (!answer || !answer[0] || !answer[0].text)
+      throw new Error('No answer found.');
+
+    res.json({ answer });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+router.get('/cfg', (req, res) => {
+  try {
+    const { PORT, APP_NAME, AUTH_TOKEN } = process.env;
+    res.json({
+      PORT,
+      APP_NAME,
+      AUTH_TOKEN,
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({ error: e });
+  }
+});
+
+router.get('/log', (req, res) => {
+  const log = logger.getLog();
+  res.json(log);
+});
+
+router.get('/', (req, res) => {
+  res.send('Hello World!' + process.env.APP_NAME);
+});
+
+module.exports = router;
